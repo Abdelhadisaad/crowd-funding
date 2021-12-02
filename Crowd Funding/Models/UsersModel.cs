3ff550d3af842f48a08d7668ca9c90278f6cafe4
Saad, Abdelhadi - Student Rijn IJssel <00293388@rijnijssel.nl>
2021-12-02 13:53:18 +0100

    hadi push

---
+++ Crowd Funding/Models/UsersModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crowd_Funding.Models
+{
+    public class UsersModel
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string phonenumber { get; set; }
+        public string role { get; set; }
+    }
+}
